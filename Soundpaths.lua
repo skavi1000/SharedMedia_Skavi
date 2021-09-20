efcc2de87dda6529580f0ca54374de4fdc44c796
skavi1000 <n4o@web4krebs.de>
2021-09-20 10:23:28 +0200

    SharedMedia for Weakauras

---
+++ Soundpaths.lua
@@ -0,0 +1,21 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF0090Impact|r", [[Interface\Addons\SharedMedia_Skavi\sound\Impact.ogg]])
+-- -----
+--   STATUSBAR
+-- -----
